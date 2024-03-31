8aac260f7c34d5eaa982df5be43d36965514f575
sourabh@32 <sourabhsharma181003@gmail.com>
2024-03-31 11:30:48 +0530

    frontend -build issue

--- frontend/src/pages/AddVariable.tsx
+++ frontend/src/pages/AddVariable.tsx
@@ -1,53 +1,53 @@
-import  {  useState } from 'react'
+import { useState } from 'react'
 import { addColor } from './ColorTable'
 
 
 type addPropType = {
-    handleAdd:({name,colorCode}:addColor)=> {}
+    handleAdd: ({ name, colorCode }: addColor) => {}
 
 }
-const AddVariable = ({handleAdd}:addPropType) => {
+const AddVariable = ({ handleAdd }: addPropType) => {
 
 
-  const [color,setColor] = useState("")
-  const [variable,setVariable] = useState("")
-
-   
+    const [color, setColor] = useState("")
+    const [variable, setVariable] = useState("")
 
 
-    
-    
-  
-
-   
 
 
-  return (
-    <tr >
-    <td className="px-6 py-4 whitespace-nowrap">
-     
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap">
 
 
-    <input placeholder='Enter color code...' type="text" className='border  py-2  border-gray-300 rounded px-4 ' onChange={(e) => setVariable( e.target.value)} value={variable} />
-    </td>
-    <td className="px-6 flex items-center gap-2 py-4 whitespace-nowrap">
 
-        <input
-            type="color"
-            value={color}
-            onChange={(e) => setColor( e.target.value)}
-            className="w-10 h-10 rounded-lg"
-        />
 
-        <input placeholder='Enter color code...' type="text" className='border  py-2  border-gray-300 rounded px-4 ' onChange={(e) => setColor( e.target.value)} value={color} />
-        <button onClick={()=>handleAdd({name:variable,colorCode:color})} className="bg-slate-800 ml-2 text-white py-2 font-bold  px-4 rounded">
-            Add
-        </button>
-    </td>
-</tr>
-  )
+
+
+
+    return (
+        <tr >
+            <td className="px-6 py-4 whitespace-nowrap">
+
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+
+
+                <input placeholder='Enter color code...' type="text" className='border  py-2  border-gray-300 rounded px-4 ' onChange={(e) => setVariable(e.target.value)} value={variable} />
+            </td>
+            <td className="px-6 flex items-center gap-2 py-4 whitespace-nowrap">
+
+                <input
+                    type="color"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                    className="w-10 h-10 rounded-lg"
+                />
+
+                <input placeholder='Enter color code...' type="text" className='border  py-2  border-gray-300 rounded px-4 ' onChange={(e) => setColor(e.target.value)} value={color} />
+                <button onClick={() => handleAdd({ name: variable, colorCode: color })} className="bg-slate-800 ml-2 text-white py-2 font-bold  px-4 rounded">
+                    Add
+                </button>
+            </td>
+        </tr>
+    )
 }
 
 export default AddVariable
--- frontend/src/pages/Schema.tsx
+++ frontend/src/pages/Schema.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import instance from '../axios';
 
 interface Props {
